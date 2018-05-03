d69479f11b2d8c66d1094e568b33c2274127bd0f
huxhu <huxhua163@163.com>
2018-05-03 14:09:23 +0800

    添加定时器

---
+++ src/main/java/com/racetime/xsad/timer/Timer.java
@@ -0,0 +1,19 @@
+package com.racetime.xsad.timer;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author hu_xuanhua_hua
+ * @ClassName: Timer
+ * @Description: TODO
+ * @date 2018-05-03 12:03
+ * @versoin 1.0
+ **/
+@Component
+public class Timer {
+    @Scheduled(cron="0/10 * *  * * ? ")   //每10秒执行一次
+    public  void goTest(){
+        System.err.println("怎么回事");
+    }
+}
