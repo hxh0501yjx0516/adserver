679ae82516d635d9e32cce00a71638e90cf90fc9
huxhu <huxhua163@163.com>
2018-05-16 10:20:51 +0800

    去掉上一版本加的判断

--- src/main/java/com/racetime/xsad/service/impl/CensusService.java
+++ src/main/java/com/racetime/xsad/service/impl/CensusService.java
@@ -60,9 +60,6 @@ public class CensusService implements ICensusService {
             }
             Set<String> launcCcountSet = shardedJedis.spop("callback", 1000);
             Map<String, Integer> insertRedisMap = new HashMap<>();
-            if (launcCcountSet != null && launcCcountSet.size() > 0) {
-
-
             for (String launcCcount : launcCcountSet) {//遍历redis中取出的数据处理整合
                 JSONObject obj = JSONObject.parseObject(launcCcount);
                 String log_data = obj.get("log_data").toString();
@@ -90,7 +87,7 @@ public class CensusService implements ICensusService {
                     }
                 }
             }
-            }
+
         } finally
 
         {
