3d71fb914af872ebe519ba2516ab0aa0383e5017
huxhu <huxhua163@163.com>
2018-06-01 17:31:55 +0800

    pmp业务新改的业务

--- src/main/java/com/racetime/xsad/dao/OrderDao.java
+++ src/main/java/com/racetime/xsad/dao/OrderDao.java
@@ -54,20 +54,22 @@ public interface OrderDao {
     /**
      * 查询库存
      */
-    List<Map<String, Object>> getResource(@Param("list") List<String> list, @Param("num") int num);
+    List<Map<String, Object>> getResource(@Param("list") List<String> list, @Param("num") int num, @Param("adx_adslot_id") String adx_adslot_id);
 
     /**
      * 插入子订单
      */
-    void insertPmp_order_detail(@Param("list") List<Map<String, Object>> list, @Param("orderid") String orderId);
+    void insertPmp_order_detail(@Param("list") List<Map<String, Object>> list, @Param("orderid") String orderId, @Param("scene_ids") String scene_ids, @Param("city_code") String city_code);
 
     /**
      * 查询pv, uv，price
      *
      * @param list
+     * @param num
+     * @param adx_adslot_id
      * @return
      */
-    Map<String, Object> getPVandUVandPRICE(@Param("list") List<String> list, @Param("num") String num);
+    Map<String, Object> getPVandUVandPRICE(@Param("list") List<String> list, @Param("num") String num, @Param("adx_adslot_id") String adx_adslot_id);
 
     /**
      * 查询物料url，所对应的客户信息
@@ -79,6 +81,12 @@ public interface OrderDao {
     void insertOrUpdateAdx_Report(@Param("pojo") Pojo pojo);
 
     List<Map<String, Object>> selectChannel_id();
+
+    List<String> getResourceId(@Param("list") List<String> list);
+
+    String getScene_id(@Param("list") List<String> list);
+
+    String getCity_code(@Param("list") List<String> list);
 
 
 }
--- src/main/java/com/racetime/xsad/service/impl/OrderService.java
+++ src/main/java/com/racetime/xsad/service/impl/OrderService.java
@@ -1,14 +1,22 @@
 package com.racetime.xsad.service.impl;
 
 import com.alibaba.druid.support.json.JSONUtils;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.racetime.xsad.dao.OrderDao;
 import com.racetime.xsad.service.IOrderService;
+import org.omg.CORBA.OBJ_ADAPTER;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
+import java.beans.BeanInfo;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
@@ -27,53 +35,33 @@ public class OrderService implements IOrderService {
     @Override
     public String getNum(String order_name) {
 
-        System.err.println("==========" + order_name);
+//        System.err.println("==========" + order_name);
         Gson gson = new GsonBuilder().enableComplexMapKeySerialization().create();
         try {
-
-
-//        Map<String, Object> map = new HashMap<>();
-//        map.put("order_name", "test");
-//        map.put("app_id", "1");
-//        map.put("ad_id", "1");
-//        map.put("customer", "1");
-//        map.put("num", "2");
-//        map.put("material", "1,2,3");
-//        map.put("edate", "2018-12-29");
-//        map.put("sdate", "2018-11-29");
-//        map.put("detail", "1,2");
-//        System.err.println(gson.toJson(map));
-            Map<String, Object> orderMap = gson.fromJson(order_name, Map.class);
-//        System.err.println(orderMap.get("detail"));
-/**
- * 获取价格
- */
-//            Map<String, Object> channelAndPriceMap = orderDao.getChannelAndPrice(orderMap.get("ad_id").toString());
+//            Map<String, Object> map1 = new HashMap<>();
+//            map1.put("channel_id", "666");
+//            map1.put("app_id", "XAP600002");
+//            map1.put("order_name", "444");
+//            map1.put("ad_id", "XAD100004");
+//            map1.put("customer", "5");
+//            map1.put("material", "1,2,3");
+//            map1.put("num", "2");
+//            map1.put("edate", "2018-12-29");
+//            map1.put("sdate", "2018-11-29");
+//            List<Map<String, String>> list1 = new ArrayList<>();
+//            Map<String, String> gomap = new HashMap<>();
+//            gomap.put("XAD100004", "1091,1092,1093");
+//            list1.add(gomap);
+//            map1.put("detail", list1);
 //
-//            String detail = orderMap.get("detail").toString();
-//            String[] details = detail.split(",");
-//            List<String> detaillist = new ArrayList<>();
-//            for (String str : details) {
-//                detaillist.add(str);
-//            }
-//            Map<String, Object> pvuvmap = orderDao.getPvUv(detaillist, Integer.parseInt(orderMap.get("num").toString()), Integer.parseInt(channelAndPriceMap.get("price").toString()));
-//            int pv = Integer.parseInt(pvuvmap.get("pv").toString());
-//            int uv = Integer.parseInt(pvuvmap.get("uv").toString());
-//            int floor_price = Integer.parseInt(pvuvmap.get("floor_price").toString());
-//            int price = Integer.parseInt(pvuvmap.get("price").toString());
-            String detail = orderMap.get("detail").toString();
-            String[] details = detail.split(",");
-            List<String> detaillist = new ArrayList<>();
-            for (String str : details) {
-                detaillist.add(str);
-            }
-            Map<String, Object> map = orderDao.getPVandUVandPRICE(detaillist, orderMap.get("num").toString());
-            Map<String, Object> resultMap = new HashMap<>();
-            resultMap.put("pv", map.get("pv"));
-            resultMap.put("uv", map.get("uv"));
-            resultMap.put("success", 200);
-            resultMap.put("price", map.get("price"));
-            resultMap.put("floor_price", map.get("floor_price"));
+//            System.err.println(gson.toJson(map1));
+//            order_name = JSON.toJSONString(map1);
+
+            JSONObject jsonObject = JSONObject.parseObject(order_name);
+            List<Map> mapList = JSON.parseArray(jsonObject.get("detail").toString(), Map.class);
+
+
+            Map<String,Object> resultMap =  getPvUvPrice(mapList,jsonObject);
             return gson.toJson(resultMap);
         } catch (Exception e) {
             e.printStackTrace();
@@ -84,24 +72,70 @@ public class OrderService implements IOrderService {
 
     }
 
+    /**
+     * 寻量
+     *
+     * @param mapList
+     * @param jsonObject
+     */
+    private Map<String,Object> getPvUvPrice(List<Map> mapList, JSONObject jsonObject) {
+        int pv = 0;
+        int uv = 0;
+        double price = 0;
+        double floor_price = 0;
+        for (Map map : mapList) {
+            Iterator entries = map.entrySet().iterator();
+            while (entries.hasNext()) {
+                Map.Entry entry = (Map.Entry) entries.next();
+                String key = entry.getKey().toString();
+                String value = entry.getValue().toString();
+                String[] ids = value.split(",");
+                List<String> resourcestockIds = new ArrayList<>();
+                for (String id : ids) {
+                    resourcestockIds.add(id);
+                }
+                Map<String, Object> pVandUVandPRICEMap = orderDao.getPVandUVandPRICE(resourcestockIds, jsonObject.getString("num"), key);
+                pv = pv + Integer.parseInt(pVandUVandPRICEMap.get("pv").toString());
+                uv = uv + Integer.parseInt(pVandUVandPRICEMap.get("uv").toString());
+                price = price + Double.parseDouble(pVandUVandPRICEMap.get("price").toString());
+                floor_price = floor_price + Double.parseDouble(pVandUVandPRICEMap.get("floor_price").toString());
+            }
+        }
+        Map<String, Object> resultMap = new HashMap<>();
+        resultMap.put("pv", pv);
+        resultMap.put("uv", uv);
+        resultMap.put("success", 200);
+        resultMap.put("price", price);
+        resultMap.put("floor_price", floor_price);
+        return  resultMap;
+    }
+
+
     @Override
     public String generatingOrder(String order_name) {
-        Gson gson = new GsonBuilder().enableComplexMapKeySerialization().create();
         try {
-
-
-//        Map<String, Object> map = new HashMap<>();
-//        map.put("order_name", "test");
-//        map.put("app_id", "1");
-//        map.put("ad_id", "1");
-//        map.put("customer", "1");
-//        map.put("num", "40");
-//        map.put("material", "1,2,3");
-//        map.put("edate", "2018-12-29");
-//        map.put("sdate", "2018-11-29");
-//        map.put("detail", "1,2");
-//        System.err.println(gson.toJson(map));
-            Map<String, Object> orderMap = gson.fromJson(order_name, Map.class);
+//            Map<String, Object> map1 = new HashMap<>();
+//            map1.put("channel_id", "666");
+//            map1.put("app_id", "XAP600002");
+//            map1.put("order_name", "444");
+//            map1.put("ad_id", "XAD100004");
+//            map1.put("customer", "5");
+//            map1.put("material", "1,2,3");
+//            map1.put("num", "2");
+//            map1.put("edate", "2018-12-29");
+//            map1.put("sdate", "2018-11-29");
+//            map1.put("floor_price", 7199.280000000001);
+//            map1.put("price", 10795.68);
+//            map1.put("uv", 12000000);
+//            map1.put("pv", 6480);
+//            List<Map<String, String>> list1 = new ArrayList<>();
+//            Map<String, String> gomap = new HashMap<>();
+//            gomap.put("XAD100004", "1091,1092,1093");
+//            list1.add(gomap);
+//            map1.put("detail", list1);
+//
+//            order_name = JSON.toJSONString(map1);
+            JSONObject jsonObject = JSONObject.parseObject(order_name);
 
             /**
              * 库存订单
@@ -128,21 +162,30 @@ public class OrderService implements IOrderService {
             /**
              * 判断库存是否是否满足生成新订单
              */
-            String detail = orderMap.get("detail").toString();
-            String[] details = detail.split(",");
-            List<String> detaillist = new ArrayList<>();
-            for (String str : details) {
-                detaillist.add(str);
+            List<String> resourcestockIds = new ArrayList<>();
+            List<Map> mapList = JSON.parseArray(jsonObject.get("detail").toString(), Map.class);
+            for (Map map : mapList) {
+                 Iterator entries = map.entrySet().iterator();
+                while (entries.hasNext()) {
+                    Map.Entry entry = (Map.Entry) entries.next();
+                    String key = entry.getKey().toString();
+                    String value = entry.getValue().toString();
+                    String[] ids = value.split(",");
+                    for (String id : ids) {
+                        resourcestockIds.add(id);
+                     }
+                }
             }
-            int num = Integer.parseInt(orderMap.get("num").toString());
-            for (int i = 0; i < detaillist.size(); i++) {
+            int num = Integer.parseInt(jsonObject.get("num").toString());
+
+            for (int i = 0; i < resourcestockIds.size(); i++) {
                 int resourceNum = 0;
                 int existingOrderNum = 0;
-                if (pmpResourceMap.get(detaillist.get(i)) != null) {
-                    resourceNum = Integer.parseInt(pmpResourceMap.get(detaillist.get(i)).toString());
+                if (pmpResourceMap.get(resourcestockIds.get(i)) != null) {
+                    resourceNum = Integer.parseInt(pmpResourceMap.get(resourcestockIds.get(i)).toString());
                 }
-                if (existingOrderMap != null && existingOrderMap.get(detaillist.get(i)) != null) {
-                    existingOrderNum = Integer.parseInt(existingOrderMap.get(detaillist.get(i)).toString());
+                if (existingOrderMap != null && existingOrderMap.get(resourcestockIds.get(i)) != null) {
+                    existingOrderNum = Integer.parseInt(existingOrderMap.get(resourcestockIds.get(i)).toString());
                 }
                 if (num > resourceNum - existingOrderNum) {
                     Map<String, Object> resultmap = new HashMap<>();
@@ -151,9 +194,9 @@ public class OrderService implements IOrderService {
                 }
 
             }
-/**
- * 获取价格
- */
+
+
+
 
             /**
              * 生成订单
@@ -162,35 +205,44 @@ public class OrderService implements IOrderService {
             String orderId = simpleDateFormat.format(new Date());
             Map<String, Object> insertOrderMap = new HashMap<>();
             insertOrderMap.put("orderId", orderId);
-            insertOrderMap.put("pv", orderMap.get("pv"));
-            insertOrderMap.put("uv", orderMap.get("uv"));
-            insertOrderMap.put("ad_app_id", orderMap.get("app_id"));
-            insertOrderMap.put("ad_position_id", orderMap.get("ad_id"));
-            insertOrderMap.put("order_name", orderMap.get("order_name"));
-            insertOrderMap.put("ad_customer_id", orderMap.get("customer"));
-            insertOrderMap.put("num", orderMap.get("num"));
-            insertOrderMap.put("floor_price", orderMap.get("floor_price"));
-            insertOrderMap.put("money", orderMap.get("price"));
-            insertOrderMap.put("material_id", orderMap.get("material"));
-            insertOrderMap.put("sdate", orderMap.get("sdate"));
-            insertOrderMap.put("edate", orderMap.get("edate"));
-            insertOrderMap.put("ad_channel_id", orderMap.get("channel_id"));
+            insertOrderMap.put("pv", jsonObject.get("pv"));
+            insertOrderMap.put("uv", jsonObject.get("uv"));
+            insertOrderMap.put("ad_app_id", jsonObject.get("app_id"));
+            insertOrderMap.put("ad_position_id", jsonObject.get("ad_id"));
+            insertOrderMap.put("order_name", jsonObject.get("order_name"));
+            insertOrderMap.put("ad_customer_id", jsonObject.get("customer"));
+            insertOrderMap.put("num", jsonObject.get("num"));
+            insertOrderMap.put("floor_price", jsonObject.get("floor_price"));
+            insertOrderMap.put("money", jsonObject.get("price"));
+            insertOrderMap.put("material_id", jsonObject.get("material"));
+            insertOrderMap.put("sdate", jsonObject.get("sdate"));
+            insertOrderMap.put("edate", jsonObject.get("edate"));
+            insertOrderMap.put("ad_channel_id", jsonObject.get("channel_id"));
             orderDao.InsertOrder(insertOrderMap);
             /**
              * 生成详情订单
              */
-            String resourceStockId = orderMap.get("detail").toString();
-            String[] resourceStockIds = resourceStockId.split(",");
-            List<String> resourceStockList = new ArrayList<>();
-            for (
-                    String str : resourceStockIds)
 
-            {
-                resourceStockList.add(str);
+            List<Map> mapList1 = JSON.parseArray(jsonObject.get("detail").toString(), Map.class);
+            for (Map map : mapList1) {
+                List<String> listid = new ArrayList<>();
+                Iterator entries = map.entrySet().iterator();
+                while (entries.hasNext()) {
+                    Map.Entry entry = (Map.Entry) entries.next();
+                    String key = entry.getKey().toString();
+                    String value = entry.getValue().toString();
+                    String[] ids = value.split(",");
+                    for (String id : ids) {
+                        listid.add(id);
+                    }
+                    List<String> resourceIdList = orderDao.getResourceId(listid);
+                    String scene_ids = orderDao.getScene_id(resourceIdList);
+                    String city_code = orderDao.getCity_code(resourceIdList);
+                    List<Map<String, Object>> resourceList = orderDao.getResource(listid, Integer.parseInt(jsonObject.get("num").toString()),key);
+                    orderDao.insertPmp_order_detail(resourceList, orderId,scene_ids,city_code);
+
+                }
             }
-
-            List<Map<String, Object>> resourceList = orderDao.getResource(resourceStockList, Integer.parseInt(orderMap.get("num").toString()));
-            orderDao.insertPmp_order_detail(resourceList, orderId);
 
             Map<String, Object> resultmap = new HashMap<>();
             resultmap.put("success", 200);
@@ -203,5 +255,9 @@ public class OrderService implements IOrderService {
         }
     }
 
+    public static void main(String[] args) {
+        Map jsonObject = new HashMap();
+        jsonObject.put("XAD100004", "1091,1092,1093");
 
+    }
 }
